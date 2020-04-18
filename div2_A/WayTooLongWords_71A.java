746437003a19bf3cd315b282ed95300cedf0cfc3
zakaria5729 <zakariahossain143@gmail.com>
2020-04-19 02:29:31 +0600

    Way Too Long Words 71A

---
+++ div2_A/WayTooLongWords_71A.java
@@ -0,0 +1,25 @@
+package div2_A;
+
+import java.util.Scanner;
+
+public class WayTooLongWords_71A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		while(num >= 0) {
+			String st = sc.nextLine();
+						
+			if(st.length() > 10) {
+				System.out.println(st.charAt(0) + "" + (st.length() - 2) + "" + st.charAt(st.length() - 1));
+			} else {
+				System.out.println(st);
+			}
+			
+			num--;
+		}
+		
+		sc.close();
+	}
+}
